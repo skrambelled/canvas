770e52b6531e498e3798e5fb4b719d8e631a78ee
skrambelled <skrambelled@gmail.com>
2020-09-25 16:21:53 -0700

    circles grow and shrink

--- canvas.js
+++ canvas.js
@@ -3,35 +3,24 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const c = canvas.getContext('2d');
+const maxSize = 100;
 
 console.log(canvas);
 
-// c.fillStyle = 'rgba(255,0,0,0.5)';
-// c.fillRect(100, 50, 50, 50);
-// c.fillStyle = 'rgba(0,255,0,0.5)';
-// c.fillRect(75, 40, 50, 50);
-// c.fillStyle = 'rgba(0,0,255,0.5)';
-// c.fillRect(80, 60, 50, 50);
-
-
-// // lines
-// c.beginPath();  // start a path unrelated to any prior actions
-// c.moveTo(50, 200);
-// c.lineTo(250, 100);
-// c.lineTo(300, 400);
-// c.strokeStyle = '#A12DD6';
-// c.stroke();
-
+var mouse = {
+  x: undefined,
+  y: undefined
+}
 
 var circleArray = [];
 
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 100; i++) {
   // arc
   let radius = Math.ceil(Math.random() * 10) * 4;
   let x = Math.floor(Math.random() * (innerWidth - radius * 2)) + radius;
   let y = Math.floor(Math.random() * (innerHeight - radius * 2)) + radius;
-  let dx = Math.ceil(Math.random() * 5) * ((Math.round(Math.random()) * 2) - 1);
-  let dy = Math.ceil(Math.random() * 5) * ((Math.round(Math.random()) * 2) - 1);
+  let dx = Math.ceil(Math.random() * 3) * ((Math.round(Math.random()) * 2) - 1);
+  let dy = Math.ceil(Math.random() * 3) * ((Math.round(Math.random()) * 2) - 1);
   circleArray.push(new Circle(x, y, dx, dy, radius));
 }
 
@@ -41,16 +30,20 @@ function Circle(x, y, dx, dy, radius) {
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.originalRadius = radius;
   this.r = Math.floor(Math.random() * 256);
   this.g = Math.floor(Math.random() * 256);
   this.b = Math.floor(Math.random() * 256);
-  this.color = 'rgb(' + this.r + ',' + this.g + ',' + this.b + ')';
+  this.a = .5;
+  this.color = this.r + ',' + this.g + ',' + this.b;
 
   this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.strokeStyle = this.color;
+    c.strokeStyle = 'rgb(' + this.color + ')';
     c.stroke();
+    c.fillStyle = 'rgba(' + this.color + ', 0.5)';
+    c.fill();
   }
 
   this.update = function () {
@@ -62,6 +55,12 @@ function Circle(x, y, dx, dy, radius) {
 
     this.x += this.dx;
     this.y += this.dy;
+
+    if (Math.abs(mouse.x - this.x) < 50 && Math.abs(mouse.y - this.y) < 50) {
+      if (this.radius < maxSize)
+        this.radius++;
+    } else if (this.radius > this.originalRadius)
+      this.radius--;
 
     this.draw();
   }
@@ -77,3 +76,13 @@ function animate() {
   }
 }
 animate();
+
+window.addEventListener('mousemove', function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener('ontouchmove', function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
